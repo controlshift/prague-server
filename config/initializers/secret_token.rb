335d8bf067516b9c82a31ce566412bd4b5c475a7
Dylan <dylandrop@gmail.com>
2014-01-10 14:25:39 -0500

    Initialized rails 4 project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PragueServer::Application.config.secret_key_base = 'a6829365653bad8a6edd640a24e90c6400aae902fe3c53b44d7518a5973f862383743eca1894fdd500d2d282c780ab221ec8ec45a32e475c970dac45fc19f062'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PragueServer::Application.config.session_store :cookie_store, key: '_prague-server_session'
