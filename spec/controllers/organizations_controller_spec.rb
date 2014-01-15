28c52255e7828b4a11ba3ce2413325926a8f3d0c
Nathan Woodhull <woodhull@gmail.com>
2014-01-15 14:30:26 -0500

    refactor organizations controller spec.

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -2,38 +2,45 @@ require 'spec_helper'
 
 describe OrganizationsController do
   describe 'POST create' do
-    it 'creates an organization with omniauth credentials' do
-      OmniAuth.config.mock_auth[:stripe_connect] = OmniAuth::AuthHash.new({
+    let(:auth_hash_hash) { OmniAuth::AuthHash.new(auth_hash) }
+    before(:each) do
+      OmniAuth.config.mock_auth[:stripe_connect] = auth_hash_hash
+      request.env["omniauth.auth"] = auth_hash
+      post :create
+    end
+
+    context 'with credentials' do
+      let(:auth_hash) { {
         'uid' => 'X',
         'info' => { 'stripe_publishable_key' => 'X' },
         'credentials' => { 'token' => 'X' }
-      })
-      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:stripe_connect] 
+      } }
 
-      post :create
-      expect(response).to render_template("create")
-      Organization.last.stripe_user_id.should == 'X'
-      Organization.last.stripe_publishable_key.should == 'X'
+      it 'creates an organization' do
+        OmniAuth.config.mock_auth[:stripe_connect] = OmniAuth::AuthHash.new()
+        request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:stripe_connect]
+
+        expect(response).to render_template("create")
+        Organization.last.stripe_user_id.should == 'X'
+        Organization.last.stripe_publishable_key.should == 'X'
+      end
     end
 
-    it 'renders new if something goes wrong with creating an organization' do
-      OmniAuth.config.mock_auth[:stripe_connect] = OmniAuth::AuthHash.new({})
-      request.env["omniauth.auth"] = {}
-      post :create
-      expect(response).to render_template("new")
-    end
+    context 'without credentials' do
+      let(:auth_hash) { {} }
 
-    it 'renders new if some OmniAuth error is raised' do
-      OmniAuth.config.mock_auth[:stripe_connect] = :access_denied
-      post :create
-      expect(response).to render_template("new")
+      it 'renders new if something goes wrong with creating an organization' do
+        expect(response).to render_template("new")
+      end
     end
   end
 
   describe 'GET new' do
+    before(:each) { get :new }
+
     it 'renders a new template for an Organization' do
-      get :new
       expect(response).to render_template("new")
     end
+    specify{ assigns(:organization).should_not be_nil}
   end
 end
