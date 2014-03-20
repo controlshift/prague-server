3f5e0fc51c2e2359fe8a77479f11f1938e8c6172
Dylan <dylandrop@gmail.com>
2014-03-20 11:49:32 -0400

    Fallback for dev or test environments for secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-PragueServer::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
+PragueServer::Application.config.secret_key_base = (Rails.env.development? || Rails.env.test?) ? ('x' * 30) : ENV['SECRET_KEY_BASE']
