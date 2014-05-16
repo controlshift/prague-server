60a6a6f30599fde1dfe11e5ab8b616a4bd02d1e2
Dylan <dylandrop@gmail.com>
2014-05-16 11:53:07 -0400

    Fix breaking specs

--- scenarios/scenarios/config_controller_spec.rb
+++ scenarios/scenarios/config_controller_spec.rb
@@ -4,6 +4,7 @@ feature ConfigController do
 
   before(:each) do
     Rails.cache.clear
+    GeoIP.any_instance.stub(:country).and_return(GeoIP::Country.new)
     stub_request(:get, 'http://platform.controlshiftlabs.com/cached_url/currencies').to_return(body: "{\"rates\":{\"GBP\":1.1234}}")
   end
 
