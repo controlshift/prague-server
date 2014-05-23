29c6ac814c3ebb4616bac1953df272af46c0edd4
Nathan Woodhull <woodhull@gmail.com>
2014-05-23 12:38:27 -0400

    display pretty flash messages. Devise uses these to notify people of signup issues.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,22 @@ module ApplicationHelper
     end
   end
 
+  def bootstrap_class_for flash_type
+    case flash_type
+      when :success
+        "alert-success" # Green
+      when :error
+        "alert-danger" # Red
+      when :alert
+        "alert-warning" # Yellow
+      when :notice
+        "alert-info" # Blue
+      else
+        flash_type.to_s
+    end
+  end
+
+
   private
 
   def title_for(page_title, options)
