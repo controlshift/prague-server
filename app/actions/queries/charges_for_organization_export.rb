f6f2092b48312328bf573e62add548dbf38c4564
Nathan Woodhull <woodhull@gmail.com>
2014-05-26 17:34:29 -0400

    support for exporting charges.

---
+++ app/actions/queries/charges_for_organization_export.rb
@@ -0,0 +1,17 @@
+module Queries
+  class ChargesForOrganizationExport < Export
+    attr_accessor :organization
+
+    def klass
+      Charge
+    end
+
+    def sql
+      Charge.where(organization_id: organization.id).joins(:customer).select(['customers.first_name', 'customers.last_name', 'customers.email', 'charges.amount', 'charges.currency', 'charges.created_at']).to_sql
+    end
+
+    def name
+      "charges-#{organization.slug}"
+    end
+  end
+end
---
+++ spec/actions/queries/charges_for_organization_export_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe Queries::ChargesForOrganizationExport do
+  let(:organization) { create(:organization) }
+
+  subject { Queries::ChargesForOrganizationExport.new organization: organization }
+
+  describe 'initialization' do
+    specify { subject.organization.should == organization }
+  end
+
+  describe 'exporting' do
+    let!(:charge) { create(:charge, organization: organization  ) }
+
+    it 'should have a header_row' do
+      subject.header_row.should include('first_name')
+    end
+
+    context 'stream as string' do
+      before(:each) do
+        @result = ""
+        subject.as_csv_stream.each do |chunk|
+          @result << chunk
+        end
+      end
+
+      context "when parsed" do
+        before(:each) do
+          @parsed = CSV.parse(@result)
+        end
+
+        describe "header" do
+          let(:header) { @parsed[0] }
+
+          specify{ header.should include("first_name") }
+          specify{ header.should include("email")}
+        end
+
+        describe "first row" do
+          let(:first_row) { @parsed[1] }
+
+          specify{ first_row.should include(charge.amount.to_s) }
+          specify{ first_row.should include(charge.customer.email)}
+        end
+      end
+    end
+  end
+end
