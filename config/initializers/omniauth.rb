66da8bd4fe1e448e6edb12447bb63a10f4b05fc4
Dylan <dylandrop@gmail.com>
2014-05-26 16:43:46 -0400

    Added rails application secrets in omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :stripe_connect, ENV['STRIPE_CONNECT_CLIENT_ID'], ENV['STRIPE_SECRET'], scope: 'read_write'
+  provider :stripe_connect, 
+    ENV['STRIPE_CONNECT_CLIENT_ID'] || Rails.application.secrets.STRIPE_CONNECT_CLIENT_ID, 
+    ENV['STRIPE_SECRET'] || Rails.application.secrets.STRIPE_SECRET, scope: 'read_write'
 end
 
 OmniAuth.config.on_failure = Proc.new do |env|
