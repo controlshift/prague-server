98089935c7efe1d5377505084885830be30b73f2
Dylan <dylandrop@gmail.com>
2014-05-26 17:09:48 -0400

    Fixed breaking scenarios

--- scenarios/support/helpers.rb
+++ scenarios/support/helpers.rb
@@ -2,8 +2,10 @@ module HelperMethods
   include Warden::Test::Helpers
   def login organization
     Warden.test_mode!
-    login_as(organization)
-    visit organization_path(organization)
+    visit new_organization_session_path
+    fill_in "organization[email]", with: organization.email
+    fill_in "organization[password]", with: "password"
+    click_button "Sign in"
     expect(page).to have_content organization.name
   end
 end
