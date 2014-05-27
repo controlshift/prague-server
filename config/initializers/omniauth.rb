5e8d4092ce07635388f6450330442533943c1938
Dylan <dylandrop@gmail.com>
2014-05-27 10:29:37 -0400

    Remove secrets.yml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :stripe_connect, 
-    ENV['STRIPE_CONNECT_CLIENT_ID'] || Rails.application.secrets.STRIPE_CONNECT_CLIENT_ID, 
-    ENV['STRIPE_SECRET'] || Rails.application.secrets.STRIPE_SECRET, scope: 'read_write'
+    ENV['STRIPE_CONNECT_CLIENT_ID'], ENV['STRIPE_SECRET'], scope: 'read_write'
 end
 
 OmniAuth.config.on_failure = Proc.new do |env|
