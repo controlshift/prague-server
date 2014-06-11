2459134f065563db1c1f67811196159f7206ad49
Nathan Woodhull <woodhull@gmail.com>
2014-06-11 17:45:44 -0400

    use appropriate key for scenario.

--- app/workers/create_customer_token_worker.rb
+++ app/workers/create_customer_token_worker.rb
@@ -14,7 +14,7 @@ class CreateCustomerTokenWorker
           card: card_token,
           description: customer.id
         },
-        organization.live? ? ENV['STRIPE_SECRET'] : ENV['STRIPE_TEST_SECRET']
+        charge.organization.live? ? ENV['STRIPE_SECRET'] : ENV['STRIPE_TEST_SECRET']
       )
       customer.update_attribute(:customer_token, stripe_customer.id)
       ChargeCustomerWorker.perform_async(charge.id)
