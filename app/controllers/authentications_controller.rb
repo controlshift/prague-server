5649d355e031a143a3aeb57d02c706f9b894429b
Nathan Woodhull <woodhull@gmail.com>
2014-06-11 00:20:38 -0400

    more debug logging.

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,5 +1,6 @@
 class AuthenticationsController < ApplicationController
   def create
+    logger.debug request.env
     current_organization.apply_omniauth(request.env['omniauth.auth'])
     if current_organization.save
       redirect_to current_organization
