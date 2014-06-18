8d878fe92dcc692b6037b6564ea089963ed02404
Dylan <dylandrop@gmail.com>
2014-06-18 12:42:47 -0400

    Fix breaking config controller test

--- scenarios/scenarios/config_controller_spec.rb
+++ scenarios/scenarios/config_controller_spec.rb
@@ -12,13 +12,15 @@ feature ConfigController do
 
   it 'should respond with default settings for JSON request' do
     visit "/config/#{organization.slug}"
-    page.body.should == organization.global_defaults.merge(rates: { 'GBP' => 1.1234}, country: nil).to_json
+    page.body.should == organization.global_defaults.merge(rates: { 'GBP' => 1.1234})
+      .merge(errors: I18n.t('errors', locale: 'en')).merge(fields: I18n.t('fields', locale: 'en')).merge(country: nil).to_json
     page.response_headers['Content-Type'].should == 'application/javascript'
   end
 
   it 'should be able to handle a blank external request' do
     stub_request(:get, 'http://platform.controlshiftlabs.com/cached_url/currencies').to_return(body: "")
     visit "/config/#{organization.slug}"
-    page.body.should == organization.global_defaults.merge(rates: nil, country: nil).to_json
+    page.body.should == organization.global_defaults.merge(rates: nil)
+      .merge(errors: I18n.t('errors', locale: 'en')).merge(fields: I18n.t('fields', locale: 'en')).merge(country: nil).to_json
   end
 end
