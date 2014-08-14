7b7a2d6717aeace7af82cd2fa2296760ac226c26
Dylan <dylandrop@gmail.com>
2014-08-14 18:25:27 -0400

    Try to reconfigure sidekiq and update it, hopefully get it working with unicorn.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,12 @@
+sk_url = ENV['REDIS_PROVIDER'] || 'redis://localhost:6379'
+sk_namespace = "prague-#{Rails.env}"
+
 Sidekiq.configure_server do |config|
-  config.redis = { :url => ENV['REDIS_PROVIDER'] || 'redis://localhost:6379', :namespace => "prague-#{Rails.env}" }
+  config.redis = { url: sk_url, namespace: sk_namespace  }
+end
+
+unless Rails.env.production? || Rails.env.staging?
+  Sidekiq.configure_client do |config|
+    config.redis = { size: 1, url: sk_url, namespace: sk_namespace  }
+  end
 end
