5a25a388d5b1a3ddf94a850f1cda61f5e890c6b6
Dylan <dylandrop@gmail.com>
2014-08-15 12:16:25 -0400

    Move config client back into sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,3 +4,13 @@ sk_namespace = "prague-#{Rails.env}"
 Sidekiq.configure_server do |config|
   config.redis = { url: sk_url, namespace: sk_namespace  }
 end
+
+Sidekiq.configure_client do |config|
+  config.redis = { :url => ENV['REDIS_PROVIDER'] || 'redis://localhost:6379', :namespace => "prague-#{Rails.env}" }
+end
+
+unless Sidekiq::VERSION < '3'
+  Sidekiq.configure_server do |config|
+    config.error_handlers << Proc.new {|ex,context| Honeybadger.notify(ex, context: context) }
+  end
+end
