6b5361608fcebb1325e1eaab9f69d2b1617ce42c
Dylan <dylandrop@gmail.com>
2014-08-15 11:49:28 -0400

    Unnecessary config in sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,9 +4,3 @@ sk_namespace = "prague-#{Rails.env}"
 Sidekiq.configure_server do |config|
   config.redis = { url: sk_url, namespace: sk_namespace  }
 end
-
-unless Rails.env.production? || Rails.env.staging?
-  Sidekiq.configure_client do |config|
-    config.redis = { size: 1, url: sk_url, namespace: sk_namespace  }
-  end
-end
