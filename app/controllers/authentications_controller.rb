3a5c3f83a82f707ae1845b7d0778360f403979a3
Jacinda Moore <alynna.kasmira@gmail.com>
2014-09-05 16:50:40 -0400

    During oauth workflow, redirect back to oauth page after sign in or sign up with stripe

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -2,7 +2,12 @@ class AuthenticationsController < ApplicationController
   def create
     current_organization.apply_omniauth(request.env['omniauth.auth'])
     if current_organization.save
-      redirect_to current_organization
+      stored_loc = stored_location_for(current_organization)
+      if stored_loc
+        redirect_to stored_loc
+      else
+        redirect_to current_organization
+      end
     else
       render :new, notice: "Something went wrong. Please try again."
     end
