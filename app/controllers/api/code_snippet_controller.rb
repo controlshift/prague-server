662b4f4dd73c3f9db3bd9055c750f9c95f76b0ea
Jacinda Moore <alynna.kasmira@gmail.com>
2014-09-23 15:38:55 -0400

    parse incoming tags param

--- app/controllers/api/code_snippet_controller.rb
+++ app/controllers/api/code_snippet_controller.rb
@@ -1,6 +1,6 @@
 class Api::CodeSnippetController < Api::BaseController
   def show
-    tags = params[:tags]
+    tags = params[:tags].split(',')
     render html: current_resource_owner.code_snippet(tags: tags).html_safe
   end
 end
