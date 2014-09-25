12164c75c521ee3599fe57ddedcbe4bfb2726692
Jacinda Moore <alynna.kasmira@gmail.com>
2014-09-25 13:44:08 -0400

    Expose code snippet parameters as api endpoint

--- app/controllers/api/code_snippet_controller.rb
+++ app/controllers/api/code_snippet_controller.rb
@@ -3,4 +3,9 @@ class Api::CodeSnippetController < Api::BaseController
     tags = params.fetch(:tags, '').split(',')
     render html: current_resource_owner.code_snippet(tags: tags).to_html.html_safe
   end
+
+  def parameters
+    tags = params.fetch(:tags, '').split(',')
+    render json: current_resource_owner.code_snippet(tags: tags).params
+  end
 end
