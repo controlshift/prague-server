58808f04e29ccae2b139903c801adf233c3450ff
Jacinda Moore <alynna.kasmira@gmail.com>
2014-10-08 16:15:12 -0400

    Add spec for currency conversion for BSD

--- spec/notifiers/blue_state_notifier_spec.rb
+++ spec/notifiers/blue_state_notifier_spec.rb
@@ -6,8 +6,25 @@ describe BlueStateNotifier do
   let!(:crm) { create(:crm, platform: 'bluestate', organization: organization) }
   let(:charge) { create(:charge, customer: customer, organization: organization) }
 
+  before :each do
+    mock_connection = double()
+    allow(mock_connection).to receive(:perform_request).and_return('{"summary": {"missing_ids": 0, "failures": 0}}')
+    BlueStateDigital::Connection.stub(:new).and_return(mock_connection)
+  end
+
   it 'should push the contribution into BSD' do
-    BlueStateDigital::Contribution.any_instance.should_receive(:save)
+    expect_any_instance_of(BlueStateDigital::Contribution).to receive(:save)
+    BlueStateNotifier.new.process(charge)
+  end
+
+  it 'should convert amounts into the default currency for the CRM' do
+    crm.default_currency = 'XYZ'
+    charge.amount = 123
+    expect(charge).to receive(:converted_amount).with('XYZ').and_return(23456)
+    expect(BlueStateDigital::Contribution).to receive(:new) do |options|
+      expect(options[:transaction_amt]).to eq(23456)
+      double().as_null_object
+    end
     BlueStateNotifier.new.process(charge)
   end
 end
