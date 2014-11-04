3ef4eb9a2ad69a884f77742e86d62087465fdae3
Jacinda Moore <alynna.kasmira@gmail.com>
2014-11-04 14:30:28 -0500

    include tags in CSV export of charges

--- app/actions/queries/charges_for_organization_export.rb
+++ app/actions/queries/charges_for_organization_export.rb
@@ -7,7 +7,23 @@ module Queries
     end
 
     def sql
-      Charge.where(organization_id: organization.id).joins(:customer).select(['customers.first_name', 'customers.last_name', 'customers.email', 'charges.amount', 'charges.currency', 'charges.config', 'charges.status', 'charges.created_at']).to_sql
+      <<-ENDOFQUERY
+      WITH charges_customers AS (
+        SELECT charges.id AS charge_id,
+               customers.id AS customer_id,
+               string_agg(tags.name, ',') AS tags
+        FROM charges
+        JOIN customers ON customers.id = charges.customer_id
+        LEFT JOIN charges_tags ON charges_tags.charge_id = charges.id
+        LEFT JOIN tags ON tags.id = charges_tags.tag_id
+        WHERE charges.organization_id = 1
+        GROUP BY charges.id, customers.id
+        )
+      SELECT customers.first_name, customers.last_name, customers.email, charges.amount, charges.currency, charges.config, charges.status, charges.created_at, charges_customers.tags
+      FROM charges_customers
+      JOIN charges ON charges.id = charges_customers.charge_id
+      JOIN customers ON customers.id = charges_customers.customer_id
+      ENDOFQUERY
     end
 
     def name
