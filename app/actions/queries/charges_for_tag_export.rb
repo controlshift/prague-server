4bcadbbf0463e4e42c7b0a6f5370acc550ff1619
Jacinda Moore <alynna.kasmira@gmail.com>
2014-11-04 17:21:22 -0500

    Add the ability to download a CSV of charges for a tag

---
+++ app/actions/queries/charges_for_tag_export.rb
@@ -0,0 +1,41 @@
+module Queries
+  class ChargesForTagExport < Export
+    attr_accessor :tag
+
+    def klass
+      Charge
+    end
+
+    def sql
+      <<-ENDOFQUERY
+      WITH charges_for_tag AS (
+        SELECT charges.id as charge_id,
+               charges.customer_id
+        FROM charges
+        JOIN charges_tags ON charges_tags.charge_id = charges.id
+        JOIN tags ON tags.id = charges_tags.tag_id
+        WHERE tags.id = #{ActiveRecord::Base.sanitize(tag.id)}
+        AND charges.organization_id = #{ActiveRecord::Base.sanitize(tag.organization.id)}
+      ),
+      charges_customers AS (
+        SELECT charges_for_tag.charge_id,
+               customers.id AS customer_id,
+               string_agg(tags.name, ',') AS tags
+        FROM charges_for_tag
+        JOIN customers ON customers.id = charges_for_tag.customer_id
+        LEFT JOIN charges_tags ON charges_tags.charge_id = charges_for_tag.charge_id
+        LEFT JOIN tags ON tags.id = charges_tags.tag_id
+        GROUP BY charges_for_tag.charge_id, customers.id
+        )
+      SELECT customers.first_name, customers.last_name, customers.email, charges.amount, charges.currency, charges.config, charges.status, charges.created_at, charges_customers.tags
+      FROM charges_customers
+      JOIN charges ON charges.id = charges_customers.charge_id
+      JOIN customers ON customers.id = charges_customers.customer_id
+      ENDOFQUERY
+    end
+
+    def name
+      "charges-#{tag.organization.slug}-#{tag.name}"
+    end
+  end
+end
---
+++ app/controllers/org/tags/charges_controller.rb
@@ -0,0 +1,15 @@
+class Org::Tags::ChargesController < Org::OrgController
+  before_filter :load_tag
+
+  def index
+    respond_to do |format|
+      format.csv do
+        streaming_csv_export(Queries::ChargesForTagExport.new(tag: @tag))
+      end
+    end
+  end
+
+  def load_tag
+    @tag = current_organization.tags.where(name: params[:tag_id]).first!
+  end
+end
