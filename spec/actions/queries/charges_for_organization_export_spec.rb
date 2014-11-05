79b4fa0d3b63a4f914199be1246f198157292245
Jacinda Moore <alynna.kasmira@gmail.com>
2014-11-05 12:47:00 -0500

    expand spec for charges_for_organization_export to include all fields

--- spec/actions/queries/charges_for_organization_export_spec.rb
+++ spec/actions/queries/charges_for_organization_export_spec.rb
@@ -11,6 +11,14 @@ describe Queries::ChargesForOrganizationExport do
 
   describe 'exporting' do
     let!(:charge) { create(:charge, organization: organization, config: {'foo' => 'bar'}) }
+    let!(:tag) { create(:tag, name: 'whales', organization: organization) }
+    let!(:namespace) { create(:tag_namespace, namespace: 'food', organization: organization)}
+    let!(:namespaced_tag) { create(:tag, name: 'food:cookies', organization: organization, namespace: namespace) }
+
+    before :each do
+      charge.tags << tag
+      charge.tags << namespaced_tag
+    end
 
     it 'should have a header_row' do
       subject.header_row.should include('first_name')
@@ -33,16 +41,28 @@ describe Queries::ChargesForOrganizationExport do
           let(:header) { @parsed[0] }
 
           specify{ header.should include("first_name") }
+          specify{ header.should include("last_name") }
           specify{ header.should include("email")}
+          specify{ header.should include("amount")}
+          specify{ header.should include("currency")}
           specify{ header.should include("config")}
+          specify{ header.should include("status")}
+          specify{ header.should include("created_at")}
+          specify{ header.should include("tags")}
         end
 
         describe "first row" do
           let(:first_row) { @parsed[1] }
 
-          specify{ first_row.should include(charge.amount.to_s) }
+          specify{ first_row.should include(charge.customer.first_name)}
+          specify{ first_row.should include(charge.customer.last_name)}
           specify{ first_row.should include(charge.customer.email)}
+          specify{ first_row.should include(charge.amount.to_s) }
+          specify{ first_row.should include(charge.currency) }
           specify{ first_row.should include('"foo"=>"bar"')}
+          specify{ first_row.should include('live') }
+          specify{ first_row.should include(charge.created_at.strftime("%F %H:%M:%S.%6N")) }
+          specify{ first_row.should include('whales,food:cookies') }
         end
       end
     end
