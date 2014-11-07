06a38475afe1a70e30f9507ccad3bc79b774a90e
mtakac <takacmarek1221@gmail.com>
2014-11-07 08:16:22 +0100

    Refactor ChargeCustomerWorker #1: Split #perform
    As first step I've split #perform into multiple methods.

--- app/workers/charge_customer_worker.rb
+++ app/workers/charge_customer_worker.rb
@@ -21,40 +21,16 @@ class ChargeCustomerWorker
     # Since we just pass in a Stripe customer identifier, it will use the customer's default card.
     # Fortunately, we just created this Stripe customer, with the card the donor entered as the default card.
     # So it will charge the card the donor entered.
-    token = Stripe::Token.create(
-      {
-        customer: charge.customer.customer_token
-      },
-      charge.live? ? charge.organization.access_token : charge.organization.stripe_test_access_token
-    )
+    token = get_stripe_token(charge)
 
     # This is where we actually charge the customer.
-    stripe_charge = Stripe::Charge.create({
-                            amount: charge.amount,
-                            currency: charge.currency,
-                            application_fee: charge.application_fee,
-                            card: token.id,
-                            metadata: {
-                              'charge_id' => charge.id,
-                              'name' => charge.customer.full_name,
-                              'email' => charge.customer.email
-                            },
-                            description: "#{Time.zone.now.to_s} - #{charge.customer.id} - #{charge.organization.slug}"
-                          },
-                          charge.live? ? charge.organization.access_token : charge.organization.stripe_test_access_token
-    )
+    stripe_charge = create_stripe_charge(charge, token)
 
     # Sweet, it worked!  Mark everything as successfully paid.
-    charge.update_attributes(paid: true, stripe_id: stripe_charge[:id], card: stripe_charge[:card].to_hash )
-    LogEntry.create(charge: charge, message: 'Successful charge.')
-
-    Pusher[charge.pusher_channel_token].trigger('charge_completed', {
-      status: 'success'
-    })
+    mark_as_paid!(charge, stripe_charge)
 
     # Schedule jobs to update the organization's CRM and send the customer a receipt.
-    CrmNotificationWorker.perform_async(charge.id)
-    ChargeNotificationMailer.delay.send_receipt(charge.id)
+    send_notifications!(charge)
 
   rescue Stripe::CardError => e
     charge.update_attributes(paid: false)
@@ -82,5 +58,47 @@ class ChargeCustomerWorker
     })
     Rails.logger.debug("StandardError #{e.message}")
     Honeybadger.notify(e, context: {charge_id: charge.id})
+  end
+
+  # Get a single-use Stripe token that we will use to run the charge.
+  # Since we just pass in a Stripe customer identifier, it will use the customer's default card.
+  # Fortunately, we just created this Stripe customer, with the card the donor entered as the default card.
+  # So it will charge the card the donor entered.
+  def get_stripe_token(charge)    
+    Stripe::Token.create({ customer: charge.customer.customer_token },
+      charge.live? ? charge.organization.access_token : charge.organization.stripe_test_access_token
+    )
+  end
+
+  # This is where we actually charge the customer.
+  def create_stripe_charge(charge, token)
+    Stripe::Charge.create({
+      amount: charge.amount,
+      currency: charge.currency,
+      application_fee: charge.application_fee,
+      card: token.id,
+      metadata: {
+        'charge_id' => charge.id,
+        'name' => charge.customer.full_name,
+        'email' => charge.customer.email
+      },
+      description: "#{Time.zone.now.to_s} - #{charge.customer.id} - #{charge.organization.slug}"
+    }, charge.live? ? charge.organization.access_token : charge.organization.stripe_test_access_token)
+  end
+
+  # Sweet, it worked!  Mark everything as successfully paid.
+  def mark_as_paid!(charge, stripe_charge)
+    charge.update_attributes(paid: true, stripe_id: stripe_charge[:id], card: stripe_charge[:card].to_hash )
+    LogEntry.create(charge: charge, message: 'Successful charge.')
+
+    Pusher[charge.pusher_channel_token].trigger('charge_completed', {
+      status: 'success'
+    })
+  end
+
+  # Schedule jobs to update the organization's CRM and send the customer a receipt.
+  def send_notifications!(charge)
+    CrmNotificationWorker.perform_async(charge.id)
+    ChargeNotificationMailer.delay.send_receipt(charge.id)
   end
 end
