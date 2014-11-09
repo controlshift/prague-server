9be7e707a32bed450b92f5e08c12d38c63cf8d48
mtakac <takacmarek1221@gmail.com>
2014-11-09 01:20:04 +0100

    Fix for wrong intendation in MarkChargeAsPaid spec

--- spec/services/mark_charge_as_paid_service_spec.rb
+++ spec/services/mark_charge_as_paid_service_spec.rb
@@ -4,7 +4,7 @@ describe MarkChargeAsPaidService do
   let(:charge) { create(:charge, card: nil, paid: false)}
   let(:stripe_charge) { { id: 1, card: JSON.parse('{"test": "test"}') }}
 
-    describe '#call' do
+  describe '#call' do
     before(:each) { MarkChargeAsPaidService.new(charge, stripe_charge).call }
 
     it 'sets charge.paid to be true' do
