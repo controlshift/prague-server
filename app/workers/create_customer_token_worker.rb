6588f0d76b320ee694cdddf3db0ddf53940f7ea5
Nathan Woodhull <woodhull@gmail.com>
2014-12-09 10:52:04 -0500

    do not validate the card while creating customer, on advice of stripe.

--- app/workers/create_customer_token_worker.rb
+++ app/workers/create_customer_token_worker.rb
@@ -12,6 +12,7 @@ class CreateCustomerTokenWorker
         email: customer.email,
         metadata: customer.to_hash,
         card: card_token,
+        validate: false,
         description: customer.id
       },
       charge.organization.live? ? ENV['STRIPE_SECRET'] : ENV['STRIPE_TEST_SECRET']
