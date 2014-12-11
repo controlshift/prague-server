7cfd7381d9e264390e609f046d67e79258da3883
Nathan Woodhull <woodhull@gmail.com>
2014-12-10 23:22:45 -0500

    migrate user data from organizations to users.

--- db/migrate/20141107224403_devise_create_users.rb
+++ db/migrate/20141107224403_devise_create_users.rb
@@ -1,4 +1,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration
+  class User < ActiveRecord::Base
+    attr_accessor :email, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :reset_password_token,
+                  :reset_password_sent_at, :encrypted_password
+  end
+
   def change
     create_table(:users) do |t|
       ## Database authenticatable
@@ -36,5 +41,13 @@ class DeviseCreateUsers < ActiveRecord::Migration
     add_index :users, :confirmation_token,   unique: true
     add_index :users, :organization_id
     # add_index :users, :unlock_token,         unique: true
+
+    execute "INSERT into users (email, encrypted_password, reset_password_token, reset_password_sent_at, remember_created_at,
+                                sign_in_count, current_sign_in_at, last_sign_in_at, current_sign_in_ip, last_sign_in_ip, confirmation_token,
+                                confirmed_at, confirmation_sent_at, unconfirmed_email, organization_id)
+             (SELECT email, encrypted_password, reset_password_token, reset_password_sent_at, remember_created_at, sign_in_count,
+                     current_sign_in_at, last_sign_in_at, current_sign_in_ip, last_sign_in_ip, confirmation_token, confirmed_at, confirmation_sent_at, unconfirmed_email, id
+              FROM organizations);"
+
   end
 end
