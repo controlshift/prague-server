ae98959aa9744b918ad87f01649758337b71084f
Nathan Woodhull <woodhull@gmail.com>
2014-12-30 18:31:28 -0500

    fix issues from @anero in charges_controller.

--- app/controllers/org/charges_controller.rb
+++ app/controllers/org/charges_controller.rb
@@ -1,17 +1,16 @@
 class Org::ChargesController < Org::OrgController
   def index
-    current_user.organization.charges.live.order('created_at DESC')
     respond_to do | format |
       format.html do
-        @charges = if current_user.organization.testmode?
-          current_user.organization.charges.test.order('created_at DESC').paginate(page: params[:page])
+        @charges = if current_organization.testmode?
+          current_organization.charges.test.order('created_at DESC').paginate(page: params[:page])
         else
-          current_user.organization.charges.live.order('created_at DESC').paginate(page: params[:page])
+          current_organization.charges.live.order('created_at DESC').paginate(page: params[:page])
         end
       end
 
       format.csv do
-        streaming_csv_export(Queries::ChargesForOrganizationExport.new(organization: current_user.organization))
+        streaming_csv_export(Queries::ChargesForOrganizationExport.new(organization: current_organization))
       end
     end
   end
