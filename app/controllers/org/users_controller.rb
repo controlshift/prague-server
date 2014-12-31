b1e2129597544306519fd2f7c23d6d3cc105a876
Nathan Woodhull <woodhull@gmail.com>
2014-12-31 09:56:29 -0500

    allow users to be deleted.

--- app/controllers/org/users_controller.rb
+++ app/controllers/org/users_controller.rb
@@ -3,4 +3,15 @@ class Org::UsersController < Org::OrgController
     @invitation = Invitation.new(organization: current_organization)
     @users = current_organization.users.paginate(per_page: 100, page: params[:page])
   end
+
+  def destroy
+    @user = current_organization.users.find(params[:id])
+
+    if @user == current_user
+      redirect_to organization_users_path(current_organization), alert: 'May not delete current user.'
+    else
+      @user.destroy
+      redirect_to organization_users_path(current_organization), notice: "#{@user.email} removed."
+    end
+  end
 end
---
+++ spec/controllers/org/users_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe Org::UsersController do
+  let(:organization) { create(:organization) }
+  let(:user) { create(:confirmed_user, organization: organization)}
+
+
+  before(:each) do
+    controller.stub(:current_organization).and_return( organization )
+    sign_in user
+  end
+
+  describe 'index' do
+    it 'should successfully load' do
+      get :index, organization_id: organization.id
+      expect(response).to be_success
+    end
+  end
+
+  describe 'destroy' do
+    let(:other_user) { create(:confirmed_user, organization: organization) }
+
+    it 'should be possible to destroy another user' do
+      delete :destroy, organization_id: organization, id: other_user
+      expect(response).to be_redirect
+      expect(assigns(:user).frozen?).to be_true
+    end
+
+    it 'should not be possible to delete the currently signed in user' do
+      delete :destroy, organization_id: organization, id: user
+      expect(response).to be_redirect
+      expect(assigns(:user).frozen?).to be_false
+    end
+  end
+end
