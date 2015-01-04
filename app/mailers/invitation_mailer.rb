dbfb01011a0fb9f42430fa4b8d93f6269efd1d67
Nathan Woodhull <woodhull@gmail.com>
2015-01-04 15:39:47 -0500

    remove improper article

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -3,6 +3,6 @@ class InvitationMailer < ActionMailer::Base
 
   def invitation_email(invitation_id)
     @invitation = Invitation.includes(:sender, :organization).find(invitation_id)
-    mail to: @invitation.recipient_email, subject: "You have been invited to join the #{@invitation.organization.name} on TakeCharge"
+    mail to: @invitation.recipient_email, subject: "You have been invited to join #{@invitation.organization.name} on TakeCharge"
   end
 end
