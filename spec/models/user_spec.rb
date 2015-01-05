20f9a53d65737a6ebe0759e96b82ee74ffd41a2c
Nathan Woodhull <woodhull@gmail.com>
2015-01-04 21:08:57 -0500

    some more spec validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,7 +26,10 @@ require 'spec_helper'
 
 describe User do
   it { should belong_to :organization }
-  it { should validate_presence_of(:email)}
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
+  it { should have_one(:invitation) }
+
 
   describe "email addresses" do
     it { should allow_value('george@washington.com').for(:email) }
