26abc900526260c707e253bb00b9ee1089583dac
Nathan Woodhull <woodhull@gmail.com>
2015-01-07 13:08:37 -0500

    add some debug logging.

--- app/workers/organization_updated_worker.rb
+++ app/workers/organization_updated_worker.rb
@@ -1,11 +1,15 @@
 class OrganizationUpdatedWorker
   include Sidekiq::Worker
 
+  KIND = 'organization.updated'
+
   def perform(id)
     organization = Organization.find(id)
-    event = { kind: 'organization.updated' }
+    event = { kind: KIND }
     organization.webhook_endpoints.each do |hook|
+      Rails.logger.debug "Notifying #{hook.url} of #{KIND}"
       HTTParty.post(hook.url, body: {event: event})
     end
   end
+
 end
