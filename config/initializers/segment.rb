c1767958e7551a06676d03338b724c7f93f94030
Nathan Woodhull <woodhull@gmail.com>
2015-03-27 11:45:20 -0400

    add require statement for segment.

--- config/initializers/segment.rb
+++ config/initializers/segment.rb
@@ -1,4 +1,6 @@
 if ENV['SEGMENT_WRITE_KEY'].present?
+  require 'segment/analytics'
+
   Analytics = Segment::Analytics.new({
                                        write_key: ENV['SEGMENT_WRITE_KEY'],
                                        on_error: Proc.new { |status, msg| print msg }
