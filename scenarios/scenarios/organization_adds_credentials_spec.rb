fffa9cb757652d739c0b2a79e1f8c1c59bbb1fb2
Nathan Woodhull <woodhull@gmail.com>
2015-06-09 15:12:04 -0400

    fix scenario.

--- scenarios/scenarios/organization_adds_credentials_spec.rb
+++ scenarios/scenarios/organization_adds_credentials_spec.rb
@@ -21,7 +21,7 @@ feature 'Organization adds CRM credentials' do
     find("li", text: "ActionKit").click
     fill_in 'crm_username', with: 'user'
     fill_in 'crm_password', with: 'password'
-    fill_in 'crm_host', with: 'host'
+    fill_in 'crm_host', with: 'www.google.com'
     fill_in 'crm_donation_page_name', with: 'page1'
     fill_in 'crm_default_currency', with: 'USD'
     find("input[type='submit']").click
@@ -31,7 +31,7 @@ feature 'Organization adds CRM credentials' do
     crm = Organization.last.crm
     expect(crm.username).to eq('user')
     expect(crm.platform).to eq('actionkit')
-    expect(crm.host).to eq('host')
+    expect(crm.host).to eq('www.google.com')
   end
 
   it 'creates BSD credentials for the first time', js: true do
@@ -43,7 +43,7 @@ feature 'Organization adds CRM credentials' do
     find("li", text: "Blue State Digital").click
     fill_in 'crm_username', with: 'API Key'
     fill_in 'crm_password', with: 'API Secret'
-    fill_in 'crm_host', with: 'host'
+    fill_in 'crm_host', with: 'www.google.com'
     fill_in 'crm_default_currency', with: 'USD'
     find("input[type='submit']").click
     wait_for_ajax
@@ -52,7 +52,7 @@ feature 'Organization adds CRM credentials' do
     crm = Organization.last.crm
     expect(crm.username).to eq('API Key')
     expect(crm.platform).to eq('bluestate')
-    expect(crm.host).to eq('host')
+    expect(crm.host).to eq('www.google.com')
     expect(crm.import_stubs).to be_empty
   end
 end
