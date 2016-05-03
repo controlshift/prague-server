1996a448c4a86184ec4d704c610ded075177b6f4
Nathan Woodhull <woodhull@gmail.com>
2016-05-03 13:47:13 -0400

    update spec infrastructure and other dependencies.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -78,4 +78,13 @@ RSpec.configure do |config|
   #     --seed 1234
   config.include FactoryGirl::Syntax::Methods
   config.order = "random"
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      # Choose a test framework:
+      with.test_framework :rspec
+
+      with.library :rails
+    end
+  end
 end
