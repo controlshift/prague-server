67bd351367127fd5a4407415cfe8a215d73795c1
Nathan Woodhull <woodhull@gmail.com>
2016-05-26 17:18:27 -0400

    explicitly set the stripe api version.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,3 +2,5 @@ StripeEvent.configure do |events|
   events.all StripeWebhook::Logger.new(Rails.logger)
   events.subscribe 'account.application.deauthorized', StripeWebhook::Deauthorized.new
 end
+
+Stripe.api_version = '2016-03-07'
